5bee4ccc152233339aaa3401ea0731a86e153f93
luckyseo <ityun777@gmail.com>
2024-12-19 01:13:23 +0900

    challenge 10/28 mission complete

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
+
 
 # Create your views here.
+def get_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(request, "show_tweet_list.html", {"tweets": tweets})

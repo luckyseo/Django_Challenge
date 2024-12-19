a4201251ae657bad4d88f5cc9bdee9301a8675b3
luckyseo <ityun777@gmail.com>
2024-12-20 03:23:26 +0900

    fixed name

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.tweets),
-    path("api/v1/tweets", views.list_all_tweets, name="list_all_tweets"),
-    path("api/v1/users/<int:user_id>/tweets", views.user_tweets, name="user_tweets"),
+    path("api/v1/tweets", views.db_tweets, name="db_tweets"),
+    path(
+        "api/v1/users/<int:user_id>/tweets",
+        views.user_generated_Tweets,
+        name="user_generated_Tweets",
+    ),
 ]

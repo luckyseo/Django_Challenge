295e743d3a6b42c58f4794d384015cdac77fa7f9
StarStriker <77044214+luckyseo@users.noreply.github.com>
2025-03-13 07:01:31 +1100

    assig 8

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Tweet
 # Create your views here.
+
+
+def get_Tweets(request):
+    tweets = Tweet.objects.all()
+    return render(request,"all_tweets.html",{"tweets":tweets})
